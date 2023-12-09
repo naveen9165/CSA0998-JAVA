aaede61d5345a22ba518d431190c51b994afd343
naveen9165 <113327327+naveen9165@users.noreply.github.com>
2023-12-09 08:36:21 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class oddeven{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        int num;
+        num=S.nextInt();
+        if(num%2==0)
+        System.out.println("even number");
+        else
+         System.out.println("odd number");
+    }
+}
