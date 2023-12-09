7a89239b5ecaf681d5a85aad11ddd9d5806752bf
naveen9165 <113327327+naveen9165@users.noreply.github.com>
2023-12-09 08:38:56 +0530

    Add files via upload

---
+++ clgname.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class clgname{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        String clgname,deptname;
+        System.out.println("enter clg name");
+        clgname=S.next();
+        System.out.println("enter dept name");
+        deptname=S.next();
+        System.out.println(clgname + deptname);
+    }
+}
