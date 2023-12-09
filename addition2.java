011ad2a8fe7f94884cc945d39962df44818dc0e1
naveen9165 <113327327+naveen9165@users.noreply.github.com>
2023-12-09 08:40:29 +0530

    Add files via upload

---
+++ addition2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class addition2{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        int a,b,c;
+        a=S.nextInt();
+        b=S.nextInt();
+        c=a+b;
+        System.out.println("additon:"+c);
+    }
+}
