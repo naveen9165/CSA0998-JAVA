1663a9b16ddfe6c70d0f576daac78e5b39d0b641
naveen9165 <113327327+naveen9165@users.noreply.github.com>
2023-12-09 08:32:24 +0530

    Add files via upload

---
+++ simpleinterst.java
@@ -0,0 +1,9 @@
+class simpleinterst
+{
+    public static void main(String arg[]){
+        float p,r,t,si;
+        p=130000;
+        r=2;
+        t=2;
+        si=(p*t*r)/100;
+        System.out.println("simple intrest ="+si)}}
